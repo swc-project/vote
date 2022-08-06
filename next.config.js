8c24bd76d7579ea8dfc7a7d61e646175ef851ddf
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-06 15:30:21 +0900

    feat: Configure mui (#3)

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+
+  experimental: {
+    modularizeImports: {
+      "@mui/material": {
+        transform: "@mui/material/{{member}}",
+      },
+      "@mui/icons-material": {
+        transform: "@mui/icons-material/{{member}}",
+      },
+    },
+  },
 };
