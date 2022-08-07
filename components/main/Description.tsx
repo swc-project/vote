3bbe8363c8215217391c9c1513575bde4748e891
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-07 13:28:20 +0900

    Description

--- components/main/Description.tsx
+++ components/main/Description.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const Description: React.FC = () => {};
+const Description: React.FC = () => {
+  return (
+    <div>
+      <p>This is an app to prioritize issues of SWC.</p>
+      <p></p>
+      <p>
+        There are lots of issues to work on, and we want to focus on real
+        user&apos;s problem.
+      </p>
+      <p>
+        You can get vote by donating via github sponsors or open collective.
+      </p>
+    </div>
+  );
+};
 
 export default Description;
